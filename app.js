2ae7e4ce06bea4d5bf429f1127b39869f9c6a18b
waleed <waleed.sukhon77@gmail.com>
2023-11-30 17:13:22 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+let data = require("./data");
+const app = express();
+const port = 3000;
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send(data);
+});
+
+app.post("/", (req, res) => {
+  data = req.body;
+  console.log(data);
+  res.send(true);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
